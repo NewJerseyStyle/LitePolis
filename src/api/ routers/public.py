b21018a979b6d908052df4d5021b702d1d2610f3
Stupid <22520563+NewJerseyStyle@users.noreply.github.com>
2024-04-30 14:49:27 -0400

    Added DB connection health chheck

--- src/api/ routers/public.py
+++ src/api/ routers/public.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
 
+from db import check_db_conn_health
+
 router = APIRouter()
 
 @router.get("/")
 async def get_testroute():
-    return "OK"
+    return "OK" if check_db_conn_health() else "DB conn failed"
