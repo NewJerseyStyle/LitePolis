05fe1a32cfabeea8320c268930df3fc3a74ef8a3
Stupid <22520563+NewJerseyStyle@users.noreply.github.com>
2024-06-14 02:01:58 -0400

    implement test cases
    implemented:
    - test_read_main()
    - test_read_secure_main()
    - test_read_main_invalid_parameter()
    - test_read_main_not_found()
    - test_read_user_role()

--- src/api/test_main.py
+++ src/api/test_main.py
@@ -6,145 +6,157 @@ import main
 client = TestClient(main.app)
 
 def test_read_main():
-    response = client.get("/")
+    response = client.get("/api/v1/public/")
     assert response.status_code == 200
     assert response.text == "OK"
 
 def test_read_secure_main():
-    response = client.get("/", headers={"X-Token": os.environ.get('API_KEY')})
+    response = client.get("/api/v1/secure/", headers={"X-API-Key": os.environ.get('API_KEY')})
     assert response.status_code == 200
-    assert response.json() == {"user_id": 0}
+    data = response.json()
+    assert "user_id" in data
+    assert data["user_id"] == 0
 
 def test_read_main_invalid_parameter():
-    response = client.get("/", headers={"X-Token": os.environ.get('API_KEY')})
+    response = client.get("/api/v1/secure/")
+    assert response.status_code == 403
+    invalid_token = os.environ.get('API_KEY') + "0"
+    response = client.get("/api/v1/secure/", headers={"X-API-Key": invalid_token})
+    assert response.status_code == 403
 
 def test_read_main_not_found():
     response = client.get("/")
+    assert response.status_code != 200
+    assert response.json() == {"detail": "Not Found"}
+
+def test_read_user_role():
+    response = client.get("/api/v1/secure/users/role", headers={"X-API-Key": os.environ.get('API_KEY')})
     assert response.status_code == 200
-    assert response.json() == {"msg": "Hello World"}
+    assert response.json() == {"user_role": "root"}
 
 def test_read_user_renew():
-    response = client.get("/", headers={"X-Token": os.environ.get('API_KEY')})
+    # TODO
+    response = client.get("/api/v1/secure/users/renew", headers={"X-API-Key": os.environ.get('API_KEY')})
     assert response.status_code == 200
     assert response.json() == {"user_id": 0}
     
 def test_read_user_profile():
-    response = client.get("/users/profile", headers={"X-Token": os.environ.get('API_KEY')})
+    response = client.get("/users/profile", headers={"X-API-Key": os.environ.get('API_KEY')})
     assert response.status_code == 200
     assert response.json() == {"user_id": 0}
 
 def test_read_user_invalid_parameter():
-    response = client.get("/users/profile", headers={"X-Token": os.environ.get('API_KEY')})
+    response = client.get("/users/profile", headers={"X-API-Key": os.environ.get('API_KEY')})
 
 def test_create_user_profile():
-    response = client.get("/users/profile", headers={"X-Token": os.environ.get('API_KEY')})
+    response = client.get("/users/profile", headers={"X-API-Key": os.environ.get('API_KEY')})
     assert response.status_code == 200
     assert response.json() == {"user_id": 0}
 
 def test_create_user_invalid_parameter():
-    response = client.get("/users/profile", headers={"X-Token": os.environ.get('API_KEY')})
+    response = client.get("/users/profile", headers={"X-API-Key": os.environ.get('API_KEY')})
 
 def test_update_user_profile():
-    response = client.get("/users/profile", headers={"X-Token": os.environ.get('API_KEY')})
+    response = client.get("/users/profile", headers={"X-API-Key": os.environ.get('API_KEY')})
     assert response.status_code == 200
     assert response.json() == {"user_id": 0}
 
 def test_update_user_invalid_parameter():
-    response = client.get("/users/profile", headers={"X-Token": os.environ.get('API_KEY')})
+    response = client.get("/users/profile", headers={"X-API-Key": os.environ.get('API_KEY')})
 
 def test_delete_user_profile():
-    response = client.get("/users/profile", headers={"X-Token": os.environ.get('API_KEY')})
+    response = client.get("/users/profile", headers={"X-API-Key": os.environ.get('API_KEY')})
     assert response.status_code == 200
     assert response.json() == {"user_id": 0}
 
 def test_delete_user_invalid_parameter():
-    response = client.get("/users/profile", headers={"X-Token": os.environ.get('API_KEY')})
+    response = client.get("/users/profile", headers={"X-API-Key": os.environ.get('API_KEY')})
 
 def test_read_all_conversation():
-    response = client.get("/conversations/all", headers={"X-Token": os.environ.get('API_KEY')})
+    response = client.get("/conversations/all", headers={"X-API-Key": os.environ.get('API_KEY')})
     assert response.status_code == 200
     assert response.json() == {"user_id": 0}
 
 def test_read_conversation():
     cid = "test"
-    response = client.get(f"/conversations/{cid}", headers={"X-Token": os.environ.get('API_KEY')})
+    response = client.get(f"/conversations/{cid}", headers={"X-API-Key": os.environ.get('API_KEY')})
     assert response.status_code == 200
     assert response.json() == {"user_id": 0}
 
 def test_read_conversation_invalid_parameter():
     cid = "test"
-    response = client.get(f"/conversations/{cid}", headers={"X-Token": os.environ.get('API_KEY')})
+    response = client.get(f"/conversations/{cid}", headers={"X-API-Key": os.environ.get('API_KEY')})
 
 def test_create_conversation():
-    response = client.post("/conversation", headers={"X-Token": os.environ.get('API_KEY')})
+    response = client.post("/conversation", headers={"X-API-Key": os.environ.get('API_KEY')})
     assert response.status_code == 200
     assert response.json() == {"user_id": 0}
 
 def test_create_conversation_invalid_parameter():
-    response = client.post("/conversation", headers={"X-Token": os.environ.get('API_KEY')})
+    response = client.post("/conversation", headers={"X-API-Key": os.environ.get('API_KEY')})
 
 def test_update_conversation():
-    response = client.get("/conversation", headers={"X-Token": os.environ.get('API_KEY')})
+    response = client.get("/conversation", headers={"X-API-Key": os.environ.get('API_KEY')})
     assert response.status_code == 200
     assert response.json() == {"user_id": 0}
 
 def test_update_conversation_invalid_parameter():
-    response = client.get("/conversation", headers={"X-Token": os.environ.get('API_KEY')})
+    response = client.get("/conversation", headers={"X-API-Key": os.environ.get('API_KEY')})
 
 def test_delete_conversation():
     cid = "test"
-    response = client.get(f"/conversation/{cid}", headers={"X-Token": os.environ.get('API_KEY')})
+    response = client.get(f"/conversation/{cid}", headers={"X-API-Key": os.environ.get('API_KEY')})
     assert response.status_code == 200
     assert response.json() == {"user_id": 0}
 
 def test_delete_conversation_invalid_parameter():
     cid = "test"
-    response = client.get(f"/conversation/{cid}", headers={"X-Token": os.environ.get('API_KEY')})
+    response = client.get(f"/conversation/{cid}", headers={"X-API-Key": os.environ.get('API_KEY')})
 
 
 
 
 def test_read_random_comments():
-    response = client.get("/comments/random", headers={"X-Token": os.environ.get('API_KEY')})
+    response = client.get("/comments/random", headers={"X-API-Key": os.environ.get('API_KEY')})
     assert response.status_code == 200
     assert response.json() == {"user_id": 0}
     
 def test_read_random_comments_invalid_parameter():
     cid = "test"
-    response = client.get(f"/conversations/{cid}", headers={"X-Token": os.environ.get('API_KEY')})
+    response = client.get(f"/conversations/{cid}", headers={"X-API-Key": os.environ.get('API_KEY')})
 
 def test_read_comments_moderate():
     cid = "test"
-    response = client.get(f"/conversations/{cid}", headers={"X-Token": os.environ.get('API_KEY')})
+    response = client.get(f"/conversations/{cid}", headers={"X-API-Key": os.environ.get('API_KEY')})
     assert response.status_code == 200
     assert response.json() == {"user_id": 0}
 
 def test_read_comments_invalid_parameter():
     cid = "test"
-    response = client.get(f"/conversations/{cid}", headers={"X-Token": os.environ.get('API_KEY')})
+    response = client.get(f"/conversations/{cid}", headers={"X-API-Key": os.environ.get('API_KEY')})
 
 def test_create_comment():
-    response = client.post("/comments", headers={"X-Token": os.environ.get('API_KEY')})
+    response = client.post("/comments", headers={"X-API-Key": os.environ.get('API_KEY')})
     assert response.status_code == 200
     assert response.json() == {"user_id": 0}
 
 def test_create_comments_invalid_parameter():
-    response = client.post("/comments", headers={"X-Token": os.environ.get('API_KEY')})
+    response = client.post("/comments", headers={"X-API-Key": os.environ.get('API_KEY')})
 
 def test_update_comments():
-    response = client.get("/comments", headers={"X-Token": os.environ.get('API_KEY')})
+    response = client.get("/comments", headers={"X-API-Key": os.environ.get('API_KEY')})
     assert response.status_code == 200
     assert response.json() == {"user_id": 0}
 
 def test_update_comments_invalid_parameter():
-    response = client.put("/comments", headers={"X-Token": os.environ.get('API_KEY')})
+    response = client.put("/comments", headers={"X-API-Key": os.environ.get('API_KEY')})
 
 def test_delete_comments():
     cid = "test"
-    response = client.get(f"/conversation/{cid}", headers={"X-Token": os.environ.get('API_KEY')})
+    response = client.get(f"/conversation/{cid}", headers={"X-API-Key": os.environ.get('API_KEY')})
     assert response.status_code == 200
     assert response.json() == {"user_id": 0}
 
 def test_delete_comments_invalid_parameter():
     cid = "test"
-    response = client.get(f"/conversation/{cid}", headers={"X-Token": os.environ.get('API_KEY')})
+    response = client.get(f"/conversation/{cid}", headers={"X-API-Key": os.environ.get('API_KEY')})
