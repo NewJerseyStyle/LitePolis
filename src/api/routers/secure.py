8b10f9b7f40201b8c3ca9183785ab21169fbb099
Stupid <22520563+NewJerseyStyle@users.noreply.github.com>
2024-06-18 01:16:25 -0400

    Update secure.py

--- src/api/routers/secure.py
+++ src/api/routers/secure.py
@@ -1,5 +1,7 @@
 import os
+import re
 import uuid
+import hashlib
 
 from fastapi import APIRouter, Depends, Request
 from fastapi import HTTPException
@@ -157,28 +159,58 @@ async def get_userprofile(user: dict = Depends(get_user)):
 @router.post("/users/profile")
 async def create_userprofile(request: Request,
                              user: dict = Depends(get_user)):
-    if False:
-        raise HTTPException(status_code=401, detail="Unauthorized")
     request_body = await request.json()
-    # sanitization!
-    new_record = Users(**request_body)
+    # sanitization
+    data = dict()
+    if 'email' in request_body:
+        # validate email format in UI
+        if re.match(r"'[^@]+@[^@]+\.[^@]+'",
+                    request_body['email']):
+            data['email'] = request_body['email']
+        else:
+            raise HTTPException(status_code=400,
+                                detail="Invalid parameter")
+        # allow config for sending validation email
+    else:
+        raise HTTPException(status_code=400, detail="Invalid parameter")
+    if 'password' in request_body:
+        data['password'] = hashlib.sha1(
+            request_body['password'].encode()).hexdigest()
+    else:
+        raise HTTPException(status_code=400, detail="Invalid parameter")
+    new_record = Users(**data)
     new_record.create()
 
 @router.put("/users/profile")
 async def update_userprofile(request: Request,
                              user: dict = Depends(get_user)):
+    if user[2] != 'user' or user[2] != 'root':
+        raise HTTPException(status_code=401, detail="Unauthorized")
     request_body = await request.json()
-    new_record = Users(**request_body)
+    data = dict()
+    data['id'] = user[0]
+    if 'email' in request_body:
+        data['email'] = request_body['email']
+    if 'password' in request_body:
+        data['password'] = hashlib.sha1(
+            request_body['password'].encode()).hexdigest()
+    new_record = Users(**data)
     new_record.update()
 
 @router.delete("/users/profile")
 async def delete_userprofile(user: dict = Depends(get_user)):
-    raise NotImplementedError
+    raise HTTPException(status_code=403, detail="Method Not Allowed")
+    # raise HTTPException(status_code=501, detail="Not Implemented")
 
 # CURD of conversation
 @router.get("/conversations/all")
 async def get_all_conversations(user: dict = Depends(get_user)):
-    return Conversations.get_all_conversation(user[0])
+    data = []
+    for con in Conversations.get_all_conversation(user[0]):
+        data.append(con.data)
+    return {
+        'detail': data
+    }
 
 @router.get("/conversations/{cid}")
 async def get_conversation(cid: int,
@@ -189,51 +221,110 @@ async def get_conversation(cid: int,
 @router.post("/conversations")
 async def create_conversation(request: Request,
                               user: dict = Depends(get_user)):
+    if user[2] != 'user':
+        raise HTTPException(status_code=401, detail="Unauthorized")
     request_body = await request.json()
-    new_record = Conversations(**request_body)
+    data = {'creator_id': user[0]}
+    if 'title' in request_body:
+        data['title'] = request_body['title']
+    else:
+        raise HTTPException(status_code=400, detail="Invalid parameter")
+    if 'description' in request_body:
+        data['desc'] = request_body['description']
+    else:
+        raise HTTPException(status_code=400, detail="Invalid parameter")
+    new_record = Conversations(**data)
     new_record.create()
 
 @router.put("/conversations")
 async def update_conversation(request: Request,
                               user: dict = Depends(get_user)):
     request_body = await request.json()
-    new_record = Conversations(**request_body)
+    data = {'creator_id': user[0]}
+    if 'title' in request_body:
+        data['title'] = request_body['title']
+    if 'description' in request_body:
+        data['desc'] = request_body['description']
+    new_record = Conversations(**data)
     new_record.update()
 
 @router.delete("/conversations/{cid}")
 async def delete_conversation(cid: int,
                               user: dict = Depends(get_user)):
-    record = Conversations(cid=cid)
-    record.delete()
+    raise HTTPException(status_code=403, detail="Method Not Allowed")
+    # raise HTTPException(status_code=501, detail="Not Implemented")
+    # record = Conversations(cid=cid)
+    # record.delete()
 
 # CURD of comments
-@router.get("/comments/{cid}/random")
+@router.get("/comments/{cid}/")
 async def get_comment(cid: int,
+                      random: bool = False,
+                      moderated: bool = False,
                       user: dict = Depends(get_user)):
-    return user
+    comment = Comments(conversation_id=cid,
+                       random=random,
+                       moderated=moderated)
+    return {'detail': comment.get_comments_from_conversation()}
 
 @router.get("/comments/{cid}/moderate")
-async def get_comments(user: dict = Depends(get_user)):
+async def get_comments(cid: int, user: dict = Depends(get_user)):
     # waiting to be moderated comments if conversation moderation enabled
-    return user
+    record = Conversations(cid=cid)
+    data = record.get_conversation_from_id()
+    if data['moderation'] is True:
+        comment = Comments(conversation_id=cid)
+        return {'detail': comment.get_comments_waiting_for_moderate()}
 
 @router.post("/comments/")
 async def create_comment(request: Request,
                          user: dict = Depends(get_user)):
+    if user[2] != 'user':
+        raise HTTPException(status_code=401, detail="Unauthorized")
     request_body = await request.json()
-    new_record = Comments(**request_body)
+    data = dict()
+    if 'comment' in request_body:
+        data['comment'] = request_body['comment']
+    else:
+        raise HTTPException(status_code=400, detail="Invalid parameter")
+    if 'user_id' in request_body:
+        data['user_id'] = request_body['user_id']
+    else:
+        raise HTTPException(status_code=400, detail="Invalid parameter")
+    if 'conversation_id' in request_body:
+        data['conversation_id'] = request_body['conversation_id']
+    else:
+        raise HTTPException(status_code=400, detail="Invalid parameter")
+    new_record = Comments(**data)
     new_record.create()
 
 @router.put("/comments/")
 async def update_comment(request: Request,
                          user: dict = Depends(get_user)):
+    if user[2] != 'user':
+        raise HTTPException(status_code=401, detail="Unauthorized")
     request_body = await request.json()
-    new_record = Comments(**request_body)
-    # or type can be approve
-    new_record.update()
+    data = dict()
+    if 'comment_id' in request_body:
+        data['comment_id'] = request_body['comment_id']
+    else:
+        raise HTTPException(status_code=400, detail="Invalid parameter")
+    if 'comment' in request_body:
+        data['comment'] = request_body['comment']
+    if 'vote' in request_body:
+        data['vote'] = request_body['vote']
+    record = Comments(**data)
+    if request_body['task'] == 'approve':
+        record.approve()
+    elif request_body['task'] == 'reject':
+        record.reject()
+    else:
+        record.update()
 
 @router.delete("/comments/{cid}")
 async def delete_comment(cid: int,
                          user: dict = Depends(get_user)):
-    record = Comments(cid)
-    record.delete()
+    raise HTTPException(status_code=403, detail="Method Not Allowed")
+    # raise HTTPException(status_code=501, detail="Not Implemented")
+    # record = Comments(cid)
+    # record.delete()
