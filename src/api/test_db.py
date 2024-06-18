8e68c8cedd6b9676c9b020578f7c424dc3c30e9e
Stupid <22520563+NewJerseyStyle@users.noreply.github.com>
2024-06-18 09:07:36 -0400

    Update test_db.py align with #6

--- src/api/test_db.py
+++ src/api/test_db.py
@@ -14,13 +14,13 @@ class TestUsers(unittest.TestCase):
     def test_create(self):
         user = Users("john.doe@example.com", "mysecretpassword", "user", 1)
         user.create()
-        self.assertEqual(Users.get_user_id_from_email("john.doe@example.com"), 1)
+        self.assertEqual(Users.get_user_id_from_email("john.doe@example.com"), {"id": 1})
 
     def test_update(self):
         user = Users("john.doe.newemail@example.com", "mynewpassword", "user", 1)
         user.update()
         self.assertEqual(Users.get_user_id_from_email("john.doe@example.com"), None)
-        self.assertEqual(Users.get_user_id_from_email("john.doe.newemail@example.com"), 1)
+        self.assertEqual(Users.get_user_id_from_email("john.doe.newemail@example.com"), {"id": 1})
 
     def test_delete(self):
         user = Users("john.doe@example.com")
@@ -42,17 +42,17 @@ class TestConversations(unittest.TestCase):
         conversation.create()
         lst = Conversations.get_all_conversation(1)
         self.assertGreater(len(lst), 0)
-        self.assertEqual(lst[0][1], 'Test Conversation') # need to check/debug
+        self.assertEqual(lst[0]["title"], 'Test Conversation')
         conversation = Conversations(cid=lst[0][0],
                                      creator_id=1,
                                      title='Updated Conversation Title')
         c = conversation.get_conversation_from_id()
         self.assertIsNotNone(c)
-        self.assertEqual(c[1], 'Test Conversation') # need to check/debug
+        self.assertEqual(c["title"], 'Test Conversation')
         conversation.update()
         c = conversation.get_conversation_from_id()
         self.assertIsNotNone(c)
-        self.assertEqual(c[1], 'Updated Conversation Title') # need to check/debug
+        self.assertEqual(c["title"], 'Updated Conversation Title')
 
     def test_delete(self):
         # conversation = Conversations(cid=1)
@@ -75,13 +75,13 @@ class TestComments(unittest.TestCase):
                            conversation_id=1,
                            comment='This is a test comment')
         comment.create()
-        self.assertEqual(comment.get_comment_from_id(1)[2], 'This is a test comment')
+        self.assertEqual(comment.get_comment_from_id(1)["comment"], 'This is a test comment')
         lst = comment.get_comments_from_conversation()
         self.assertGreater(len(lst), 0)
-        self.assertEqual(lst[0][2], 'This is a test comment')
+        self.assertEqual(lst[0]["comment"], 'This is a test comment')
         lst = comment.get_comments_waiting_for_moderate()
         self.assertGreater(len(lst), 0)
-        self.assertEqual(lst[0][2], 'This is a test comment')
+        self.assertEqual(lst[0]["comment"], 'This is a test comment')
         comment.data['id'] = id
         comment.data['moderated'] = True
         comment.update()
@@ -112,13 +112,13 @@ class TestAPI_Keys(unittest.TestCase):
         api_key = API_Keys(apikey='my_api_key', user_id=1)
         api_key.create()
         # verify that the API key is created in the database
-        self.assertEqual(api_key.get_user_id_from_apikey(), 1)
+        self.assertEqual(api_key.get_user_id_from_apikey(), {"id": 1})
 
     def test_update(self):
         api_key = API_Keys(apikey='my_api_key2', user_id=1)
         api_key.update()
         # verify that the API key is updated in the database
-        self.assertEqual(api_key.get_user_id_from_apikey(), 1)
+        self.assertEqual(api_key.get_user_id_from_apikey(), {"id": 1})
         api_key_old = API_Keys(apikey='my_api_key', user_id=1)
         self.assertEqual(api_key_old.get_user_id_from_apikey(), None)
 
