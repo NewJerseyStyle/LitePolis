89f64da4c9b8aa15eb38a6172d5e5a33f6be0872
Stupid <22520563+NewJerseyStyle@users.noreply.github.com>
2024-06-28 19:23:04 -0400

    Update main.py with basic docstring

--- src/api/main.py
+++ src/api/main.py
@@ -1,3 +1,29 @@
+"""This module defines the FastAPI application and includes routers for public and secure APIs.
+
+Routers
+-------
+
+* `public`: The public API router, available at `/api/v1/public`.
+* `secure`: The secure API router, available at `/api/v1/secure`, which requires authentication.
+
+Dependencies
+------------
+
+* `auth.py`: A module that manage user login.
+
+Getting Started
+---------------
+To run the API, execute the following command:
+$ SQLUSER="root" SQLPASS="mysecret" SQLHOST="localhost" SQLPORT="9030" ui="streamlit" fastapi dev main.py --host 0.0.0.0 --port 8000
+
+This will start the API on `http://0.0.0.0:8000` in development mode.
+
+Notes
+-----
+    The API documentation is available at `http://0.0.0.0:8000/docs` through Swagger.
+"""
+
+
 from fastapi import FastAPI, Depends
 
 from routers import secure, public
