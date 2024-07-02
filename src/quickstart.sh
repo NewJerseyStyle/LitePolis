12af1a6790dc555c53c4b4f03a7bdcc12419a83a
Stupid <22520563+NewJerseyStyle@users.noreply.github.com>
2024-07-02 16:47:12 -0400

    Update quickstart.sh

--- src/quickstart.sh
+++ src/quickstart.sh
@@ -2,8 +2,9 @@ docker build -t litepolis/api api/
 
 docker network create litepolis-net
 
-docker run --net litepolis-net -p 9030:9030 -d -p 8030:8030 -p 8040:8040 \
-                               --name quickstart starrocks/allin1-ubuntu
+docker run -p 9030:9030 -p 8030:8030 -p 8040:8040 -itd \
+                                   --net litepolis-net \
+                                   --name quickstart starrocks/allin1-ubuntu
 
 until [ "`docker inspect -f {{.State.Running}} quickstart`"=="true" ]; do
     sleep 3;
@@ -11,7 +12,7 @@ done;
 
 docker exec -i quickstart \
             mysql -P 9030 \
-            -h localhost -u root \
+            -h 127.0.0.1 -u root \
             --prompt="StarRocks > " < db.sql
 
 docker run --net litepolis-net --name api -d -p 8000:8000 \
