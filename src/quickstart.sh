2943a32f33d3f9dbb19acaca7ac2a61ad93e6684
Stupid <22520563+NewJerseyStyle@users.noreply.github.com>
2024-07-02 11:37:02 -0400

    Update quickstart.sh

--- src/quickstart.sh
+++ src/quickstart.sh
@@ -6,11 +6,11 @@ docker run --net litepolis-net -p 9030:9030 -d -p 8030:8030 -p 8040:8040 \
                                --name quickstart starrocks/allin1-ubuntu
 
 until [ "`docker inspect -f {{.State.Running}} quickstart`"=="true" ]; do
-    sleep 0.1;
+    sleep 3;
 done;
 
 docker exec -i quickstart \
-            mysql -P 9030 -h 127.0.0.1 -u root --prompt="StarRocks > " < db.sql
+            mysql -P 9030 -h quickstart -u root --prompt="StarRocks > " < db.sql
 
 docker run --net litepolis-net --name api -d -p 8000:8000 \
                               -e SQLUSER="root" \
