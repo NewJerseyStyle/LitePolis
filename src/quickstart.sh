1b7661ab996ff74e6f86dcb54172b5a589fe7579
Stupid <22520563+NewJerseyStyle@users.noreply.github.com>
2024-07-02 10:31:24 -0400

    Update quickstart.sh

--- src/quickstart.sh
+++ src/quickstart.sh
@@ -1,1 +1,15 @@
+docker network create litepolis-net
 
+docker run --net litepolis-net -p 9030:9030 -d -p 8030:8030 -p 8040:8040 \
+                               --name quickstart starrocks/allin1-ubuntu
+
+until [ "`docker inspect -f {{.State.Running}} quickstart`"=="true" ]; do
+    sleep 0.1;
+done;
+
+docker run --net litepolis-net --name api -d -p 8000:8000 \
+                              -e SQLUSER="root" \
+                              -e SQLPASS="" \
+                              -e SQLHOST=quickstart \
+                              -e API_KEY="e54d4431-5dab-474e-b71a-0db1fcb9e659" \
+                              litepolis/api
