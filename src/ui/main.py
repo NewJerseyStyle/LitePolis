915e497da9d54b2fbb756a66f1ac6573d65d4e7a
Stupid <22520563+NewJerseyStyle@users.noreply.github.com>
2024-07-13 14:46:39 -0400

    Update main.py

--- src/ui/main.py
+++ src/ui/main.py
@@ -2,8 +2,13 @@ import streamlit as st
 from streamlit_cookies_controller import CookieController
 
 import litepolis_client
+from litepolis_client.models.user_profile import UserProfile
+from litepolis_client.rest import ApiException
 
 controller = CookieController()
+configuration = litepolis_client.Configuration(
+    host = "http://localhost:8000"
+)
 
 # login
 tab1, tab2 = st.tabs(["Join conversation", "Login or register"])
@@ -18,6 +23,25 @@ with tab1:
 with tab2:
     if controller.get('litepolis.ac.apikey'):
         pass # use client SDK
+        configuration.api_key['APIKeyHeader'] = os.environ["API_KEY"]
+        configuration.api_key_prefix['APIKeyHeader'] = 'Bearer'
+
+########## need to re-gen the SDK
+# Enter a context with an instance of the API client
+with litepolis_client.ApiClient(configuration) as api_client:
+    # Create an instance of the API class
+    api_instance = litepolis_client.UserApi(api_client)
+    user_profile = litepolis_client.UserProfile() # UserProfile | 
+
+    try:
+        # Create Userprofile
+        api_response = api_instance.create_userprofile_api_v1_secure_users_profile_post(user_profile)
+        print("The response of UserApi->create_userprofile_api_v1_secure_users_profile_post:\n")
+        pprint(api_response)
+    except Exception as e:
+        print("Exception when calling UserApi->create_userprofile_api_v1_secure_users_profile_post: %s\n" % e)
+########## need to re-gen the SDK before procceed
+
         # if db query st.session_state["email"] role is "root":
         #     st.switch_page("pages/dashboard.py")
         # else:
