d685a5020d493dd06fea89656f8f5a82bd831b17
Stupid <22520563+NewJerseyStyle@users.noreply.github.com>
2024-07-16 19:55:34 -0400

    Update main.py

--- src/ui/main.py
+++ src/ui/main.py
@@ -1,3 +1,6 @@
+import hashlib
+import base64
+import json
 import streamlit as st
 from streamlit_cookies_controller import CookieController
 
@@ -15,45 +18,77 @@ tab1, tab2 = st.tabs(["Join conversation", "Login or register"])
 # input conversation id
 with tab1:
     with st.form("join_conversation"):
-        st.text_input('The conversation ID#', '')
+        b64 = st.text_input('The conversation ID#', '')
         if st.form_submit_button('Submit my picks'):
             # use client SDK access API get comments of conversation
-            litepolis_client.ConversationApi.
+            decoded_data = base64.b64decode(b64).decode('utf-8')
+            json_data = json.loads(decoded_data)
+            conversation_id = json_data.get('id')
+            if conversation_id is None:
+                st.error('Invalid ID')
+            configuration.api_key['APIKeyHeader'] = os.environ["API_KEY"]
+            configuration.api_key_prefix['APIKeyHeader'] = 'Bearer'
+            with litepolis_client.ApiClient(configuration) as api_client:
+                api_instance = conversations_api.ConversationsApi(api_client)
+                api_response = api_instance.get_conversation_api_v1_secure_conversations_cid_get(conversation_id)
+                if api_response.status_code == 200:
+                    print(api_response)
+            st.session_state['conversation_id'] = conversation_id
+            if not controller.get('litepolis.ac.apikey'):
+                # not login create api key goto portal
+                pass
             st.switch_page("pages/portal.py")
 with tab2:
     if controller.get('litepolis.ac.apikey'):
-        pass # use client SDK
-        configuration.api_key['APIKeyHeader'] = os.environ["API_KEY"]
+        st.session_state['apikey'] = controller.get('litepolis.ac.apikey')
+        configuration.api_key['APIKeyHeader'] = controller.get('litepolis.ac.apikey')
         configuration.api_key_prefix['APIKeyHeader'] = 'Bearer'
+        # Enter a context with an instance of the API client
+        with litepolis_client.ApiClient(configuration) as api_client:
+            # Create an instance of the API class
+            api_instance = litepolis_client.UserApi(api_client)
+            try:
+                api_response = api_instance.get_userrole_api_v1_secure_users_role_get()
+                if api_response["detail"]["role"] is "root":
+                    st.switch_page("pages/dashboard.py")
+                else:
+                    st.switch_page("pages/portal.py")
+            except Exception as e:
+                print("Exception when calling UserApi->create_userprofile_api_v1_secure_users_profile_post: %s\n" % e)
 
-########## need to re-gen the SDK
-# Enter a context with an instance of the API client
-with litepolis_client.ApiClient(configuration) as api_client:
-    # Create an instance of the API class
-    api_instance = litepolis_client.UserApi(api_client)
-    user_profile = litepolis_client.UserProfile() # UserProfile | 
-
-    try:
-        # Create Userprofile
-        api_response = api_instance.create_userprofile_api_v1_secure_users_profile_post(user_profile)
-        print("The response of UserApi->create_userprofile_api_v1_secure_users_profile_post:\n")
-        pprint(api_response)
-    except Exception as e:
-        print("Exception when calling UserApi->create_userprofile_api_v1_secure_users_profile_post: %s\n" % e)
-########## need to re-gen the SDK before procceed
-
-        # if db query st.session_state["email"] role is "root":
-        #     st.switch_page("pages/dashboard.py")
-        # else:
-        #     st.switch_page("pages/portal.py")
     with st.form("login"):
         # login
         email = st.text_input('email', 'email-address@domain.com')
         passwd = st.text_input('password', '********', type="password")
         # client SDK login fail
-        key = "" #litepolis_client.UserApi.verify return detail
-        if key is None:
-            # registration
-            # ask for type again password + 1username
-        else:
-            controller.set('litepolis.ac.apikey', key)
+        passwd = hashlib.md5(passwd.encode())hexdigest()
+        configuration.api_key['APIKeyHeader'] = os.environ["API_KEY"]
+        configuration.api_key_prefix['APIKeyHeader'] = 'Bearer'
+        with litepolis_client.ApiClient(configuration) as api_client:
+            # Create an instance of the API class
+            api_instance = litepolis_client.UserApi(api_client)
+            user_profile = litepolis_client.UserProfile(email=email,
+                                                        password=passwd)
+            if st.form_submit_button('login or register'):
+                try:
+                    # Create Userprofile
+                    api_response = api_instance.get_userauth_api_v1_secure_users_auth_post(user_profile)
+                    if api_response.status_code == 200:
+                        key = api_response["detail"]
+                        controller.set('litepolis.ac.apikey', key)
+                    elif api_response.status_code == 403:
+                        # # registration
+                        # passwd2 = st.text_input('Re-enter the same password',
+                        #                         placeholder='********',
+                        #                         type="password")
+                        # if not passwd == passwd2:
+                        #     st.warning('Entered two passwords not match')
+                        api_response = api_instance.create_user(user_profile)
+                        key = api_response["detail"]
+                        controller.set('litepolis.ac.apikey', key)
+                        st.session_state['apikey'] = key
+                    else:
+                        st.error('Wrong username or password')
+                except Exception as e:
+                    print("Exception when calling UserApi->create_userprofile_api_v1_secure_users_profile_post: %s\n" % e)
+    
